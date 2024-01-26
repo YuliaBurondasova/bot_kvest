a8440218561892abb4c12aaef37cb21ebe2b92f5
YuliaBurondasova <157499509+YuliaBurondasova@users.noreply.github.com>
2024-01-26 18:01:34 +0500

    Update bottest.py

--- bottest.py
+++ bottest.py
@@ -1,52 +1,83 @@
 import telebot
 import json
 
-# Загрузка данных из JSON-файла
+# загружаем данные о прогрессе пользователя из json-файла
+with open("progress.json", "r") as file:
+    progress = json.load(file)
+
+# записываем прогресс пользователя в файл
+def save_progress(chat_id, question):
+    if chat_id not in progress:
+        progress[chat_id] = {}
+    progress[chat_id]["question"] = question
+
+    with open("progress.json", "w") as file:
+        json.dump(progress, file)
+
+# получаем прогресс пользователя из файла
+def get_progress(chat_id):
+    if chat_id in progress:
+        return progress[chat_id]["question"]
+    else:
+        return None
+
+# удаляем прогресс пользователя из файла
+def delete_progress(chat_id):
+    if chat_id in progress:
+        del progress[chat_id]
+
+    with open("progress.json", "w") as file:
+        json.dump(progress, file)
+
+# загружаем данные о локациях из json-файла
 with open('locations.json', 'r', encoding='utf-8') as file:
     locations = json.load(file)
 
-bot = telebot.TeleBot("6905459458:AAHg1WrobA8UuhyZw9NAWEtlzNiQ7hZmnKE")  # Заменить на свой API-токен
+# создаем экземпляр бота
+bot = telebot.TeleBot('6905459458:AAHg1WrobA8UuhyZw9NAWEtlzNiQ7hZmnKE')
+
+# словарь, в котором будем хранить текущие локации игроков
+players = {}
 
 @bot.message_handler(commands=['start'])
 def start_game(message):
     chat_id = message.chat.id
-    # Отправка приветственного сообщения
-    bot.send_message(chat_id, "Добро пожаловать в игру 'Выживание на необитаемом острове'!")
-    # Отправка описания первой локации и вариантов действий
-    send_location(chat_id, "локация0")
+    # отправка приветственного сообщения
+    bot.send_message(chat_id, "добро пожаловать в игру 'выживание на необитаемом острове'!")
+# создаем нового игрока и начинаем игру с первой локации
+    players[chat_id] = {'current_location': 'локация0'}
+    show_location(chat_id)
 
-@bot.message_handler(func=lambda message: True)
-def process_message(message):
-    chat_id = message.chat.id
-    # Получение выбранного варианта из текста сообщения
-    option_text = message.text
-    # Поиск локации, связанной с выбранным вариантом
-    location_id = find_location_by_option(chat_id, option_text)
-    # Если локация найдена, отправка описания и вариантов действий этой локации
-    if location_id:
-        send_location(chat_id, location_id)
-    else:
-        # Отправка сообщения об ошибке, если вариант не найден
-        bot.send_message(chat_id, "Неправильный выбор, попробуйте еще раз.")
+# обработчик для кнопок с вариантами ответа игрока
+@bot.callback_query_handler(func=lambda call: True)
+def handle_button_click(call):
+    chat_id = call.message.chat.id
 
-def find_location_by_option(chat_id, option_text):
-    # Поиск локации по выбранному варианту действия
-    for location_id, location_data in locations.items():
-        if 'options' in location_data and option_text in location_data['options']:
-            return location_data['options'][option_text]
-    return None
+    # обрабатываем выбор игрока
+    player = players[chat_id]
+    player['current_location'] = call.data
 
-def send_location(chat_id, location_id):
-    # Отправка описания и вариантов действий локации
-    location_data = locations[location_id]
-    description = location_data['description']
-    options = location_data['options']
-    # Генерация списка кнопок для вариантов
-    buttons = [telebot.types.KeyboardButton(text=option_text) for option_text in options.keys()]
-    # Создание разметки клавиатуры с кнопками
-    keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(*buttons)
-    # Отправка описания и клавиатуры с вариантами
-    bot.send_message(chat_id, description, reply_markup=keyboard)
+    # отображаем новую локацию
+    show_location(chat_id)
 
+# функция для отображения текущей локации
+def show_location(chat_id):
+    player = players[chat_id]
+    location = locations[player['current_location']]
+    with open(location['photo'], 'rb') as photo_file:
+        bot.send_photo(chat_id, photo_file, caption=location['description'])
+
+    # отправляем сообщение с описанием локации
+    bot.send_message(chat_id, location['description'])
+
+    # создаем inline-клавиатуру с кнопками для выбора действия
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    for option, destination in location['options'].items():
+        button = telebot.types.InlineKeyboardButton(text=option, callback_data=destination)
+        keyboard.add(button)
+
+    bot.send_message(chat_id, "Если хотите начать квест заново нажмите кнопку /start "
+                              "Выберите действие:", reply_markup=keyboard)
+
+# запускаем бота
 bot.polling()
